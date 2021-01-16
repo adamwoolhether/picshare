3607e3d571a52c95ab29f45945b78b1b6f31da08
adamwoolhether <adamwoolhether@gmail.com>
2021-01-16 18:06:37 +0800

    added contact page

--- main.go
+++ main.go
@@ -7,10 +7,15 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html") //you can test "text/plain" to see result
-	fmt.Fprint(w, "<h1>Welcome the the picapp site</h1>")
+	//fmt.Fprint(w, r.URL.Path)
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome the the picapp site</h1>")
+	} else if r.URL.Path == "/contact" {
+	fmt.Fprint(w, "<a>Contact me: <a href=\"mailto:adamwoolhether@gmail.com\">adamwoolhether@gmail.com</a></h1>")
+	}
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe("localhost:3000", nil) 
+	http.ListenAndServe("localhost:3000", nil)
 }
