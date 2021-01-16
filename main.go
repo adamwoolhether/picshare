c1caddc60d42a688bb098c908df6b0cfd57c9a55
adamwoolhether <adamwoolhether@gmail.com>
2021-01-16 19:36:51 +0800

    implement gorilla mux router

--- main.go
+++ main.go
@@ -3,22 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html") //you can test "text/plain" to see result
-	//fmt.Fprint(w, r.URL.Path)
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome the the picapp site</h1>")
-	} else if r.URL.Path == "/contact" {
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome the the picapp site</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<a>Contact me: <a href=\"mailto:adamwoolhether@gmail.com\">adamwoolhether@gmail.com</a></h1>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>This page doesn't exist :o</h1><p>Email me if you keep getting this message.</p>")
-	}
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe("localhost:3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe("localhost:3000", r)
 }
