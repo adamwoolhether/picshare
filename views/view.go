7b23e399ce2d4e6c3beda2c023d1c72114a8badc
adamwoolhether <adamwoolhether@gmail.com>
2021-01-24 13:41:30 +0800

    implemented view type in main.go

--- views/view.go
+++ views/view.go
@@ -6,13 +6,12 @@ import "html/template"
 // on each .gohtml file specified within the function's parameters.
 // The templatized files will be appended to the parameterized 'files' list.
 func NewView(files ...string) *View {
-	files = append(files, "/views/layouts/footer.gohtml")
-
+	files = append(files, "views/layouts/footer.gohtml")
+	
 	t, err := template.ParseFiles(files...)
 	if err != nil {
-	panic(err)
+		panic(err)
 	}
-
 	return &View{
 		Template: t,
 	}
