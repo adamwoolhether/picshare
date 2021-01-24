f31f200dd29aae5b19dac629edc7b9f6e1c01d56
adamwoolhether <adamwoolhether@gmail.com>
2021-01-24 12:43:20 +0800

    added a view type to render templates

---
+++ views/view.go
@@ -0,0 +1,22 @@
+package views
+
+import "html/template"
+
+// NewView sets the template for shared components to be used
+// on each .gohtml file specified within the function's parameters.
+// The templatized files will be appended to the parameterized 'files' list.
+func NewView(files ...string) *View {
+	files = append(files, "/views/layouts/footer.gohtml")
+
+	t, err := template.ParseFiles(files...)
+	if err != nil {
+	panic(err)
+	}
+	return &View{
+		Template: t,
+	}
+}
+
+type View struct {
+	Template *template.Template
+}
