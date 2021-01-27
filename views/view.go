16f4530c8285157a84d600ed64d833a77881c128
adamwoolhether <adamwoolhether@gmail.com>
2021-01-27 20:30:53 +0800

    created boostrap layout

--- views/view.go
+++ views/view.go
@@ -5,8 +5,12 @@ import "html/template"
 // NewView sets the template for shared components to be used
 // on each .gohtml file specified within the function's parameters.
 // The templatized files will be appended to the parameterized 'files' list.
-func NewView(files ...string) *View {
-	files = append(files, "views/layouts/footer.gohtml")
+func NewView(layout string, files ...string) *View {
+	// always include the following appended layouts
+	files = append(files,
+		"views/layouts/footer.gohtml",
+		"views/layouts/bootstrap.gohtml",
+		)
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -14,9 +18,11 @@ func NewView(files ...string) *View {
 	}
 	return &View{
 		Template: t,
+		Layout: layout,
 	}
 }
 
 type View struct {
 	Template *template.Template
+	Layout	string
 }
