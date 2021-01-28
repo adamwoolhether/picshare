5a01f7540139a91bf0ec192c83fbc617b7cff6ad
adamwoolhether <adamwoolhether@gmail.com>
2021-01-28 18:08:41 +0800

    Cleaned up view.go code

--- views/view.go
+++ views/view.go
@@ -1,17 +1,21 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"path/filepath"
+)
+
+var (
+	LayoutDir   string = "views/layouts/"
+	TemplateExt string = ".gohtml"
+)
 
 // NewView sets the template for shared components to be used
 // on each .gohtml file specified within the function's parameters.
 // The templatized files will be appended to the parameterized 'files' list.
 func NewView(layout string, files ...string) *View {
 	// always include the following appended layouts
-	files = append(files,
-		"views/layouts/footer.gohtml",
-		"views/layouts/bootstrap.gohtml",
-		"views/layouts/navbar.gohtml",
-		)
+	files = append(files, layoutFiles()...)
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -19,11 +23,21 @@ func NewView(layout string, files ...string) *View {
 	}
 	return &View{
 		Template: t,
-		Layout: layout,
+		Layout:   layout,
 	}
 }
 
 type View struct {
 	Template *template.Template
-	Layout	string
+	Layout   string
+}
+
+// layoutFiles returns a slice of strings representing
+// all layout files used in PicApp
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
