8a2d36cc1ca5a356c3dc61102301b9fdd33397f2
adamwoolhether <adamwoolhether@gmail.com>
2021-02-11 00:09:04 +0800

    gorm auto migration and logs testing

--- exp/main.go
+++ exp/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"os"
+	"strings"
 )
 
 const (
@@ -17,16 +21,46 @@ type User struct {
 	gorm.Model
 	Name  string
 	Email string `gorm:"not null;uniqueIndex"`
+	Color string
 }
 
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
 		host, port, user, dbname)
 	// Verify driver name and datasource name are working correctly
-	db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 	if err != nil {
 		panic(err)
 	}
 	//db.Migrator().DropTable(&User{})
-	//db.AutoMigrate(&User{})
+	db.AutoMigrate(&User{})
+
+	name, email, color := getInfo()
+	u := User {
+		Name: name,
+		Email: email,
+		Color: color,
+	}
+
+	if err = db.Create(&u).Error; err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", u)
+}
+
+func getInfo() (name, email, color string) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("What's your name?")
+	name, _ = reader.ReadString('\n')
+	fmt.Println("What's your email?")
+	email, _ = reader.ReadString('\n')
+	fmt.Println("What's your favorite color?")
+	color, _ = reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+	email = strings.TrimSpace(email)
+	color = strings.TrimSpace(color)
+
+	return name, email, color
 }
