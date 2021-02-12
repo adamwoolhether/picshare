9c4de999dc73c417025174fe951b3a0d1f48bd91
adamwoolhether <adamwoolhether@gmail.com>
2021-02-12 22:11:50 +0800

    created the UserService for interacting with User model

--- exp/main.go
+++ exp/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
+	"picapp/models"
 )
 
 const (
@@ -14,60 +12,16 @@ const (
 	dbname = "picapp"
 )
 
-type User struct {
-	gorm.Model
-	Name  string
-	Email string `gorm:"not null;uniqueIndex"`
-	Color string
-	Orders []Order
-}
-
-type Order struct {
-	gorm.Model
-	UserID	uint
-	Amount 	int
-	Description string
-}
-
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
 		host, port, user, dbname)
-	// Verify driver name and datasource name are working correctly
-	db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
-	})
+	us, err := models.NewUserService(psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-	//db.Migrator().DropTable(&User{})
-	db.AutoMigrate(&User{}, &Order{})
-
-	var u User
-	// Make sure to preload the orders
-	if err := db.Preload("Orders").First(&u).Error; err != nil {
-		panic(err)
-	}
-	//createOrder(db, u, 1001, "Fake Description #1")
-	//createOrder(db, u, 9999, "Fake Description #2")
-	//createOrder(db, u, 100, "Fake Description #3")
-	fmt.Println(u)
-	fmt.Println(u.Orders)
-
-	// Another method:
-	var users []User
-	if err = db.Preload("Orders").Find(&users).Error; err != nil {
-		panic(err)
-	}
-	fmt.Println(users)
-
-}
-
-func createOrder(db *gorm.DB, user User, amount int, desc string) {
-	err := db.Create(&Order{
-		UserID: user.ID,
-		Amount: amount,
-		Description: desc,
-	}).Error
+	//us.DestructiveReset()
+	user, err := us.ByID(2)
+	fmt.Println(user)
 	if err != nil {
 		panic(err)
 	}
--- models/users.go
+++ models/users.go
@@ -1,9 +1,60 @@
 package models
 
 import (
+	"errors"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	_ "gorm.io/driver/postgres"
+	"gorm.io/gorm/logger"
 )
+
+var (
+	// ErrNotFound is returned when a resource isn't found in the database.
+	ErrNotFound = errors.New("models: resource not found")
+)
+
+func NewUserService(connectionInfo string) (*UserService, error) {
+	db, err := gorm.Open(postgres.Open(connectionInfo), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &UserService{
+		db: db,
+	}, nil
+}
+
+type UserService struct {
+	db *gorm.DB
+}
+
+// ByID looks up a user based on given ID.
+// If user is found, return user, nil. If user isn't found, return nil, ErrNotFound
+// A different error will return nil, otherError. Any err other than ErrNotFound will likely result in 500 error.
+func (us *UserService) ByID(id uint) (*User, error) {
+	var user User
+	err := us.db.Where("id = ?", id).First(&user).Error
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
+}
+
+/*// Closing the DB ***not sure about this method***
+func (us *UserService) Close() error {
+	return us.db.Close
+}*/
+
+// DestructiveReset drops the user table and rebuilds it.
+func(us *UserService) DestructiveReset() {
+	us.db.Migrator().DropTable(&User{})
+	us.db.AutoMigrate(&User{})
+}
 
 type User struct {
 	gorm.Model
