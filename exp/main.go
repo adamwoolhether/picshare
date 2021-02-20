c0c413218a09ce25d015eb3a119b6ec0fd0bc81a
adamwoolhether <adamwoolhether@gmail.com>
2021-02-20 16:36:31 +0800

    created rand package to generate random strings and remember tokens)

--- exp/main.go
+++ exp/main.go
@@ -2,47 +2,10 @@ package main
 
 import (
 	"fmt"
-	"picapp/models"
-)
-
-const (
-	host   = "localhost"
-	port   = 5432
-	user   = "adam"
-	dbname = "picapp"
+	"picapp/rand"
 )
 
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
-		host, port, user, dbname)
-	us, err := models.NewUserService(psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	//us.DestructiveReset()
-
-	user := models.User{
-		Name: "nikki liao",
-		Email: "nikki@cat.com",
-	}
-	if err := us.Create(&user); err != nil {
-		panic(err)
-	}
-	//user.Email = "jon@newemail.com"
-	//if err := us.Update(&user); err != nil {
-	//	panic(err)
-	//}
-	//userByEmail, err := us.ByEmail("jon@newemail.com")
-	//if err != nil {
-	//	panic(err)
-	//}
-	//fmt.Println(userByEmail)
-	if err := us.Delete(user.ID); err != nil {
-		panic(err)
-	}
-	userByID, err := us.ByID(user.ID)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(userByID)
+	fmt.Println(rand.String(10))
+	fmt.Println(rand.RememberToken())
 }
---
+++ rand/strings.go
@@ -0,0 +1,33 @@
+package rand
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
+
+const RememberTokenBytes = 32
+
+// Bytes will generate n random bytes or return an error if none is given.
+// This uses crypto/rand package, so it's safe for remember tokens.
+func Bytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b); if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+// String generates a a byte slice of size nBytes
+// and returns a string that is base64 URL encoded version of that byte slice.
+func String(nBytes int) (string, error) {
+	b, err := Bytes(nBytes); if err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(b), nil
+}
+
+// RememberToken is a helper function that generates
+// remember tokens of a predetermined byte size.
+func RememberToken() (string, error) {
+	return String(RememberTokenBytes)
+}
