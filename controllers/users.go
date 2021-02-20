4b372756a0c626cf4afde90e48c3dd5b51c00614
adamwoolhether <adamwoolhether@gmail.com>
2021-02-20 13:07:16 +0800

    Created login form

--- controllers/users.go
+++ controllers/users.go
@@ -11,26 +11,28 @@ import (
 // If templates are incorrectly parsed, a panic will occur.
 func NewUsers(us *models.UserService) *Users {
 	return &Users{
-		NewView: views.NewView("bootstrap", "users/new"),
-		us: us,
+		NewView:   views.NewView("bootstrap", "users/new"),
+		LoginView: views.NewView("bootstrap", "users/login"),
+		us:        us,
 	}
 }
 
 type Users struct {
-	NewView *views.View
-	us *models.UserService
+	NewView   *views.View
+	LoginView *views.View
+	us        *models.UserService
 }
 
-// New renders the form allowing users to create a new account
-// GET /signup
-func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-	if err := u.NewView.Render(w, nil); err != nil {
-		panic(err)
-	}
-}
+//// New renders the form allowing users to create a new account
+//// GET /signup
+//func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+//	if err := u.NewView.Render(w, nil); err != nil {
+//		panic(err)
+//	}
+//}
 
 type SignupForm struct {
-	Name 	string	`scheme:"name"`
+	Name     string `scheme:"name"`
 	Email    string `schema:"email"`
 	Password string `schema:"password"`
 }
@@ -43,14 +45,30 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	user := models.User{
-		Name: form.Name,
-		Email: form.Email,
+		Name:     form.Name,
+		Email:    form.Email,
 		Password: form.Password,
-
 	}
-	err := u.us.Create(&user); if err != nil {
+	err := u.us.Create(&user)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	fmt.Fprintln(w, user)
+}
+
+type LoginForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
+// Login verifies the provided email-addy & password, logging in the user if correct.
+// POST /login
+func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
+	form := LoginForm{}
+	if err := parseForm(r, &form); err != nil {
+		panic(err) // Implement better error handling for user
+	}
+	// Do something
+	fmt.Fprintln(w, form)
 }
--- main.go
+++ main.go
@@ -21,7 +21,6 @@ func main() {
 		host, port, user, dbname)
 	us, err := models.NewUserService(psqlInfo)
 	must(err)
-
 	//us.DestructiveReset()
 	us.AutoMigrate()
 
@@ -31,8 +30,10 @@ func main() {
 	r := mux.NewRouter()
 	r.Handle("/", staticC.Home).Methods("GET")
 	r.Handle("/contact", staticC.Contact).Methods("GET")
-	r.HandleFunc("/signup", usersC.New).Methods("GET")
+	r.Handle("/signup", usersC.NewView).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
+	r.Handle("/login", usersC.LoginView).Methods("GET")
+	r.HandleFunc("/login", usersC.Login).Methods("POST")
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe("localhost:3000", r)
 }
