39ce9db16029996a8109e5e7b9471007fc068e0f
adamwoolhether <adamwoolhether@gmail.com>
2021-02-21 20:53:12 +0800

    created HMAC type for easier string hashing

---
+++ hash/hmac.go
@@ -0,0 +1,31 @@
+package hash
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"hash"
+)
+
+
+// NewHMAC creates and returns a new HMAC object.
+func NewHMAC(key string) HMAC {
+	h := hmac.New(sha256.New, []byte(key))
+	return HMAC {
+		hmac: h,
+	}
+}
+
+// HMAC is a wrapper around crypto/hmac, easing the use of it in this code.
+type HMAC struct {
+	hmac hash.Hash
+}
+
+// Hash will has the provided input string using HMAC with
+// the secret key provided during HMAC object creation
+func (h HMAC) Hash(input string) string {
+	h.hmac.Reset()
+	h.hmac.Write([]byte(input))
+	b := h.hmac.Sum(nil)
+	return base64.URLEncoding.EncodeToString(b)
+}
