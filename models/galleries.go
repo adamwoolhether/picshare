9bcef46d17db3af2f21dcf31b22d6d98a9543e85
adamwoolhether <adamwoolhether@gmail.com>
2021-03-04 20:16:28 +0800

    began Services type and GalleryService implementation

--- models/galleries.go
+++ models/galleries.go
@@ -1,10 +1,18 @@
-package galleries
+package models
 
 import "gorm.io/gorm"
 
 type Gallery struct {
 	gorm.Model
-	userID uint `gorm:"not null;index"`
-	Title string `gorm:"not null"`
+	userID uint   `gorm:"not null;index"`
+	Title  string `gorm:"not null"`
 	// needs a slug
+}
+
+type GalleryService interface {
+	GalleryDB
+}
+
+type GalleryDB interface {
+	Create(gallery *Gallery) error
 }
---
+++ models/services.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func NewServices(connectionInfo string) (*Services, error) {
+	db, err := gorm.Open(postgres.Open(connectionInfo),
+		&gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	}); if err != nil {
+		return nil, err
+	}
+	return &Services{}, nil //TODO input data to be returned
+}
+
+type Services struct {
+	Gallery GalleryService
+	User    UserService
+}
