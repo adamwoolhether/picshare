3ec38de432416dece3fb57c9b0164a86331c20a8
adamwoolhether <adamwoolhether@gmail.com>
2021-04-05 20:10:40 +0800

    Added delete action to galleries controller and model

--- models/galleries.go
+++ models/galleries.go
@@ -1,6 +1,8 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type Gallery struct {
 	gorm.Model
@@ -17,6 +19,7 @@ type GalleryDB interface {
 	ByID(id uint) (*Gallery, error)
 	Create(gallery *Gallery) error
 	Update(gallery *Gallery) error
+	Delete(uint) error
 }
 
 func NewGalleryService(db *gorm.DB) GalleryService {
@@ -51,6 +54,14 @@ func (gv *galleryValidator) Update(gallery *Gallery) error {
 	return gv.GalleryDB.Update((gallery))
 }
 
+// Delete will delete the gallery associated with the provided ID.
+func (gv *galleryValidator) Delete(id uint) error {
+	if id <= 0 {
+		return ErrIDInvalid
+	}
+	return gv.GalleryDB.Delete(id)
+}
+
 func (gv *galleryValidator) userIDRequired(g *Gallery) error {
 	if g.UserID <= 0 {
 		return ErrUserIDRequired
@@ -59,7 +70,7 @@ func (gv *galleryValidator) userIDRequired(g *Gallery) error {
 }
 
 func (gv *galleryValidator) titleRequired(g *Gallery) error {
-	if g.Title == ""{
+	if g.Title == "" {
 		return ErrTitleRequired
 	}
 	return nil
@@ -86,6 +97,10 @@ func (gg *galleryGorm) Update(gallery *Gallery) error {
 	return gg.db.Save(gallery).Error
 }
 
+func (gg *galleryGorm) Delete(id uint) error {
+	gallery := Gallery{Model: gorm.Model{ID: id}}
+	return gg.db.Delete(&gallery).Error
+}
 
 type galleryValFunc func(*Gallery) error
 
